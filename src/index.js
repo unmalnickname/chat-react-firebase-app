365c1a64476b323847ab03dabee5620166f560e7
Luis Avila <a4288177@gmail.com>
2018-10-14 19:52:25 -0400

    The navegation bar has being made now

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { render } from 'react-dom';
 
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-light bg-primary ">
+                    <a className="navbar-brand text-white">Chat React</a>
+                </nav>
+
+            </div>
+        )
+    }
+}
+
+
+
+
+
 render(
-    <h1>Hola avengers</h1>,
+    <App />,
     document.getElementById('app')
 )
