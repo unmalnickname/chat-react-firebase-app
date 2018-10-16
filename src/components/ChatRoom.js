8c80244683c9f9111c02ac7962ead7be3d1851e8
Luis Avila <a4288177@gmail.com>
2018-10-16 16:44:47 -0400

    the firebase was create and add to the project

--- src/components/ChatRoom.js
+++ src/components/ChatRoom.js
@@ -2,32 +2,62 @@ import React, { Component } from 'react';
 
 class ChatRoom extends Component {
     constructor() {
-        super()
+        super();
+        this.updateMessage = this.updateMessage.bind(this);
+        this.submitMessage = this.submitMessage.bind(this);
         this.state = {
             message: '',
             messages: [
-                { id: 1, text: 'Hola' },
-                { id: 2, text: 'que tal' }
+                { id: 0, text: 'Hola,' },
+                { id: 1, text: 'Avengers' }
+
             ]
         }
+    }
+    updateMessage(e) {
+        this.setState({
+            message: e.target.value
+        })
+        // console.log(this.state.message);
+
+    }
+    submitMessage() {
+        const message = {
+            id: this.state.messages.length,
+            text: this.state.message
+        }
+
+        let listMessages = this.state.messages;
+        listMessages.push(message)
+        this.setState({
+            messages: listMessages
+        })
+        this.setState({ message: '' })
+
     }
     render() {
         const currentMessages = this.state.messages.map((message, i) => {
             return (
-                <li className="list-group-item list-group-item-action" key={message.id}>{message.text}</li>
+                <li key={message.id} className="list-group-item">{message.text}</li>
             )
         })
 
         return (
             <div className="card">
                 <div className="card-body">
-                    <ul className="list-group-item">
-                        {currentMessages}
+                    <ul className="list-group">                        {currentMessages}
                     </ul>
                 </div>
                 <div className="card-footer">
-                    <input className="form-control" type="text" placeholder="Write A Message " />
-                    <button className="btn btn-primary btn-block">Send Message</button>
+                    <input
+                        value={this.state.message}
+                        onChange={this.updateMessage}
+                        className="form-control"
+                        type="text"
+                        placeholder="Write A Message " />
+                    <button
+                        onClick={this.submitMessage}
+                        className="btn btn-primary btn-block">Send Message</button>
                 </div>
             </div>
         )
